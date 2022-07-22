14df3226b06668f25acf0b517d5b3a3fdf3daf3c
Sakshi Suresh Bhokare <81751306+Sakshibhokare@users.noreply.github.com>
2022-07-22 15:14:00 +0530

    Create findKthSmallestElement.cpp

---
+++ findKthSmallestElement.cpp
@@ -0,0 +1,31 @@
+int solve(BinaryTreeNode<int>* root, int& i, int k){
+//     base case
+    if(root==NULL){
+        return -1;
+    }
+    
+//     L
+    int left=solve(root->left,i,k);
+    if(left!=-1){
+        return left;
+    }
+    
+//     N
+    i++;
+    if(i==k)
+        return root->data;
+    
+//     R
+    return solve(root->right,i,k);
+}
+int kthSmallest(BinaryTreeNode<int>* root, int k) {
+    // Write your code here.
+//     inoreder is always sorted then then return the kth element 
+//     tc=0(n)
+    
+//     inorder LNR
+    
+    int i=0;
+    int ans=solve(root,i,k);
+    return ans;
+}
