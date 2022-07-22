60a7a0101c00afaae632748c2ef9976273763949
Sakshi Suresh Bhokare <81751306+Sakshibhokare@users.noreply.github.com>
2022-07-22 15:15:24 +0530

    Create partialBST.cpp

---
+++ partialBST.cpp
@@ -0,0 +1,19 @@
+bool isBST(BinaryTreeNode<int> *root, int min, int max){
+    if(root==NULL){
+        return true;
+    }
+    
+    if(root->data>=min && root->data<=max){
+        bool left=isBST(root->left, min, root->data);
+        bool right=isBST(root->right, root->data, max);
+        return left && right;
+    }
+    
+    return false;
+}
+bool validateBST(BinaryTreeNode<int> *root) {
+    // Write your code here
+//     inorder of bst is always sorted 
+    return isBST(root,INT_MIN,INT_MAX);
+    
+}
