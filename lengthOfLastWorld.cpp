f8cd8892549ac25b83f4d0e37ae2c68be352bf89
Sakshi Suresh Bhokare <81751306+Sakshibhokare@users.noreply.github.com>
2022-07-23 22:53:13 +0530

    Create lengthOfLastWorld.cpp

---
+++ lengthOfLastWorld.cpp
@@ -0,0 +1,9 @@
+int count=0;
+        reverse(s.begin(),s.end());
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' ')
+                count++;
+                      else if(count>0)
+            return count;
+        }
+        return count;
